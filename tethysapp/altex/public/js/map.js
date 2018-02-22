9e0e233913a0790e04593ad0ee27a4bc8263ee3a
SarvaPulla <sarvapulla@gmail.com>
2018-02-22 11:42:27 -0600

    Debug

--- tethysapp/altex/public/js/map.js
+++ tethysapp/altex/public/js/map.js
@@ -68,7 +68,7 @@ var LIBRARY_OBJECT = (function() {
 
         var jason2_layer = new ol.layer.Image({
             source: new ol.source.ImageWMS({
-                url: 'http://tethys.servirglobal.net:8181/geoserver/wms',
+                url: 'https://tethysdev.servirglobal.net:8181/geoserver/wms',
                 params: {'LAYERS':'altex:Jason2_Ground_Track'},
                 serverType: 'geoserver',
                 crossOrigin: 'Anonymous'
