8acf4ce293ebea2cde7864b8a6eecfe1ba16c4af
kmarkert <kel.markert@gmail.com>
2018-07-16 18:33:46 -0500

    added ocean mask to water layer

--- tethysapp/altex/geeutils.py
+++ tethysapp/altex/geeutils.py
@@ -1,4 +1,5 @@
 import ee
+import math
 
 try:
     ee.Initialize()
@@ -14,12 +15,29 @@ except EEException as e:
 geom = ee.Geometry.Rectangle([-180,-90,180,90])
 
 
+def extractS3Water(img):
+    qa = img.select('quality_flags');
+    waterBit = int(math.pow(2,31))
+    waterFlag = qa.bitwiseAnd(waterBit).eq(0)
+    return waterFlag.rename('water')
+
+
 def historicalMap(algorithm='JRC',aoi=geom):
 
+    ic = ee.ImageCollection('COPERNICUS/S3/OLCI').filterDate('2017-01-01','2017-01-31')
+
+    icWater = ic.map(extractS3Water)
+
+    oceanObs = icWater.sum()
+    allObs = icWater.count()
+
+    ocean = oceanObs.divide(allObs).multiply(100)
+    
     if algorithm in ['JRC']:
         jrc = ee.Image('JRC/GSW1_0/GlobalSurfaceWater')
-        occurrence = jrc.select('occurrence')
-        water = occurrence.clip(aoi).rename('water').updateMask(occurrence.gt(15))
+        occurrence = jrc.select('occurrence').unmask().rename('water')
+        oceanInland = occurrence.add(ocean)
+        water = oceanInland.updateMask(oceanInland.gt(15))
         waterMap = water.visualize(min=0,max=100,bands='water',palette='#e8e8e8,#00008b').getMapId()
 
     else:
