c3d184a902e7f5569cb95bb9b1353a28d84f7cbf
kmarkert <kel.markert@gmail.com>
2018-07-21 09:28:27 -0500

    code clean up

--- tethysapp/altex/jason.py
+++ tethysapp/altex/jason.py
@@ -71,7 +71,6 @@ def iqrFilter(series, position):
     lowerBound = q1 - (iqr * 1.5)
     upperBound = q3 + (iqr * 1.5)
 
-    print(iqr, lowerBound,upperBound)
 
     mask = np.where((series >= lowerBound) & (series <= upperBound))
 
@@ -82,61 +81,6 @@ def cleanData(series,position):
     nanMask = np.where(np.isnan(series)==False)
     return series[nanMask],position[nanMask]
 
-
-def outlierRemoval(series, position, clusterRange=5, interCluster=0.3):
-    series, position = cleanData(series, position)
-    # print 1,series,position
-    heights, position = iqrFilter(series, position)
-    # print 2,heights,position
-
-    diff = np.ptp(heights, axis=0)
-    labels = None
-
-    if heights.size > 5:
-
-        while diff > clusterRange:
-            kmeans = KMeans(init='k-means++', n_clusters=2, n_init=10)
-
-            X = np.vstack([heights.ravel(), heights.ravel()]).T
-
-            kmeans.fit(X)
-            clusters = kmeans.cluster_centers_.squeeze()[:, 0]
-
-            labels = kmeans.labels_
-
-            class1 = np.where(labels==0)
-            class2 = np.where(labels==1)
-
-            if len(class1[0]) < len(class2[0]):
-                idx = class1
-            else:
-                idx = class2
-
-            heights = heights[idx]
-            position = position[idx]
-            diff = np.abs(clusters[0] - clusters[1])
-
-
-        clusterMean = heights.mean()
-
-        std = heights.std()
-
-        while std > interCluster:
-            dist = np.abs(heights - clusterMean)
-            furthestIdx = np.argmax(dist)
-            keep = np.where(heights != heights[furthestIdx])
-            heights = heights[keep]
-            position = position[keep]
-
-            std = heights.std()
-
-        kmeans = None
-        finalSeries, finalPosition = iqrFilter(heights, position)
-
-    else:
-        finalSeries, finalPosition = heights, position
-
-    return finalSeries, finalPosition
 
 def filter_outlier(results):
     Range = len(results[1])
@@ -172,8 +116,6 @@ def filter_outlier(results):
             position = position[idx]
             diff = np.abs(clusters[0] - clusters[1])
 
-            print len(heights)
-
         clusterMean = heights.mean()
 
         std = heights.std()
@@ -204,7 +146,6 @@ def stretchDim(oldArr,newDim,type='nearest'):
 def parse_netCDF(args):
 
     file, lat_range, counter = args['file'], args['lat_range'], args['counter']
-    print '------------------------Start of file:'+str(file)+'------------------------------'
     data = Dataset(file)
 
     # time = data.variables['time'][:]
@@ -219,8 +160,6 @@ def parse_netCDF(args):
 
     newLat = stretchDim(lat,dim20Hz,'linear')
 
-    print 'LatDiff: {0}'.format(np.mean(lat_20hz-newLat))
-
     if float(lat_range[0]) > float(lat_range[1]):
         tmp = lat_range[0]
         lat_range[0] = lat_range[1]
@@ -228,22 +167,8 @@ def parse_netCDF(args):
 
     latidx = np.where((newLat >= float(lat_range[0])) & (newLat <= float(lat_range[1])))
     latidx_2d = np.where((lat_20hz >= float(lat_range[0])) & (lat_20hz <= float(lat_range[1])))
-    print 'Latidx before processing',latidx
-    print 'Latidx 2d',latidx_2d[0]
-    # if len(latidx[0]) == 0:
-    #     offset = latidx_2d[0]%20
-    #     # print 'Offset',offset
-    #     # print 'Latidx 2d',latidx_2d[0]
-    #
-    #     if 0 in offset:
-    #         latidx = np.array([latidx_2d[0][offset.tolist().index(0)] / 20])
-    #     else:
-    #         latidx = np.array([(latidx_2d[0][0] - offset[0]) / 20])
-
 
     if len(latidx_2d) > 0:
-        # latidx_2d = latidx_2d[0]
-
         # 1Hz VARIABLES
         alt_state_flag_ku_band_status = stretchDim(data.variables['alt_state_flag_ku_band_status'],dim20Hz,'nearest')[latidx_2d]
         model_dry_tropo_corr = stretchDim(data.variables['model_dry_tropo_corr'],dim20Hz,'linear')[latidx_2d]
@@ -271,7 +196,6 @@ def parse_netCDF(args):
         latd = []
         bs = []
 
-        print 'Len of alt 20hz',len(alt_20hz)
         if len(alt_state_flag_ku_band_status) > 0:
             if len(alt_20hz) > len(alt_state_flag_ku_band_status)*20:
                 leftover = leftover + 20
@@ -292,10 +216,6 @@ def parse_netCDF(args):
             hght.append(alt_20hz[i] - (media_corr + ice_range_20hz_ku[i]) - gc - 0.7)
             bs.append(ice_sig0_20hz_ku[i])
 
-        print 'Length of height',len(hght)
-        print 'Corrected height list:',hght
-        # Check to make sure that the distance between is the points is sufficient
-        # if len(latd) > 0:
         if len(hght) > 0:
             epoch = datetime.datetime(1970,1,1,0,0,0,0)
             ref = datetime.datetime(2000,1,1,0,0,0,0)
@@ -303,12 +223,10 @@ def parse_netCDF(args):
 
             time = ref + datetime.timedelta(seconds=trackTime)
 
-            print(time)
             time_stamp = (time-epoch).total_seconds() * 1000
 
             ht = np.array(hght)
             tIndex = np.full(ht.shape, int(counter))
-            print '-------------------------------End of file---------------------------------'
             return int(time_stamp), ht.astype(np.float), tIndex.astype(np.uint)  # Timestamp and height for Highcharts
         else:
             return None
@@ -318,13 +236,9 @@ def calc_jason_ts(lat1,lat2,start_date,end_date,track,sensor):
     ts_plot = []
 
     height = []
-    testun = []
     dt = []
     counter = 0
-    gHtArray = np.array([])
     testHt = []
-    testT = []
-    gTArray = np.array([])
     fList = []
 
     try:
@@ -364,85 +278,28 @@ def calc_jason_ts(lat1,lat2,start_date,end_date,track,sensor):
                 if track == pass_num:
                     if f_start_date <= pass_start_date <= pass_end_date <= f_end_date:
 
-                        # try:
                         args = {'file': os.path.join(working_dir,file), 'lat_range': [lat1,lat2], 'counter': counter}
                         results = parse_netCDF(args)
                         if results:
-                            # hgt,pos = outlierRemoval(results[1],results[2])
-                            testerOut = filter_outlier(results)
                             hout = np.mean(testerOut[0])
                             if np.isnan(hout) == False:
-                                testun.append(testerOut)
-
-
                                 dt.append(results[0])
-                                # test = np.append(test,hgt.mean())
-                                gHtArray = np.append(gHtArray,results[1])
-                                gTArray = np.append(gTArray, results[2])
-
                                 testHt.append(np.mean(testerOut[0]))
-                                testT.append(testerOut[1])
                                 fList.append([os.path.join(working_dir,file),[lat1,lat2],counter])
                                 counter += 1
 
-                            # except Exception: # If it returns NULL, move on to the next file.
-                            #     continue
-
-
-                        # ts_plot.append([time_stamp,round(float(hgt),3)]) # Return this to the frontend
-
-
-
-    # try:
-    gHtArray = gHtArray.astype(np.float)
-    gTArray = gTArray.astype(np.uint)
     testHt = np.array(testHt,dtype=np.float)
-    # except ValueError:
-    #     # pass
-    #     outH = []
-    #     outT = []
-    #     for i in range(gHtArray.size):
-    #         try:
-    #             outH.append(np.float(gHtArray[i]))
-    #             outT.append(np.uint(gTArray[i]))
-    #         except:
-    #             pass
-    #     gHtArray = np.array(outH,dtype=np.float)
-    #     gTArray = np.array(outT,dtype=np.uint)
 
     if len(dt) > 0:
-        # print 'Original',gHtArray,gTArray
-        print 'Raw data',dt
-        print 'Len raw data',len(dt)
-        series, position = outlierRemoval(gHtArray, gTArray)
-        print 'After Outlier removal',series,position
-        print 'Len after outlier removal',len(series),len(position)
-        ts_plot = groupObs(series,position,dt,start_date,end_date)
-        print 'Difference of outlier removal:',len(gHtArray) - len(series),len(gTArray) - len(position)
-        print 'Percentage of original:',float(float(len(series))/float(len(gHtArray)))
-        print 'Final processed',ts_plot
-        # htFlat = [val for sublist in testHt for val in sublist]
-        # tFlat = [val for sublist in testT for val in sublist]
-        # htFlat,tFlat = iqrFilter(np.array(htFlat), np.array(tFlat))
-        finalH, finalT = iqrFilter(np.array(testHt),np.array(dt))
-        ts_plot2 = zip(*[finalT,finalH])#groupObs(np.array(htFlat), np.array(tFlat), dt, start_date, end_date)
-        print 'After new removal process',ts_plot2
-        print 'Final len:',len(ts_plot),len(ts_plot2)
 
-        print 'Len of files processed:',len(fList)
-        with open("plot.csv", 'wb') as resultFile:
-            wr = csv.writer(resultFile, dialect='excel')
-            wr.writerows(ts_plot)
-            # wr.writerows(ts_plot2)
-        with open("plot2.csv", 'wb') as resultFile:
-            wr = csv.writer(resultFile, dialect='excel')
-            # wr.writerows(ts_plot)
-            wr.writerows(ts_plot2)
-        # print "Min:{0}\tMax:{1}\tMean:{2}\tStd:{3}".format(np.nanmin(test),np.nanmax(test),np.nanmean(test),np.nanstd(test))
-        # with open('/home/dev/altex.csv', 'a') as f:
-        #     writer = csv.writer(f)
-        #     writer.writerow([track,start_date,end_date,len(gHtArray),len(series),len(gHtArray) - len(series),float(float(len(series))/float(len(gHtArray))),len(ts_plot),len(fList)])
-    return ts_plot2
+        finalH, finalT = iqrFilter(np.array(testHt),np.array(dt))
+        ts_plot = zip(*[finalT,finalH])
+
+        # with open("plot2.csv", 'wb') as resultFile:
+        #     wr = csv.writer(resultFile, dialect='excel')
+        #     wr.writerows(ts_plot2)
+
+    return ts_plot
 
 # t1 = datetime.datetime.now()
 
